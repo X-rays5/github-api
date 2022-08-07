1cb861da97fe60522bb659d19cb31aba0f70244c
X-rays5 <61073708+X-rays5@users.noreply.github.com>
2022-08-07 16:55:38 +0200

    cache all requests for 30 seconds

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import { Router } from 'itty-router';
 import {GetPublicActivity} from "./activity/endpoint";
+import {Env} from "./env";
+import {JsonResponse} from "./util";
 
 // @ts-ignore
 const router: Router = new Router();
--- src/util.ts
+++ src/util.ts
@@ -1,8 +1,14 @@
-export function JsonResponse(body: any): Response {
+export function JsonResponse(body: any, status_code?: number): Response {
+    if (!status_code) {
+        status_code = 200;
+    }
+
     return new Response(JSON.stringify(body), {
         headers: {
             'Content-Type': 'application/json',
-            'Access-Control-Allow-Origin': '*'
-        }
+            'Access-Control-Allow-Origin': '*',
+            'Cache-Control': 'max-age=30'
+        },
+        status: status_code
     });
 }
